24bcbca7c3bf13add1d862d229871cf8eda2a600
gabriel <gabpavao@outlook.com>
2023-12-17 11:16:00 -0300

    feat: create middleware to handle global errors

--- express-api/src/app.ts
+++ express-api/src/app.ts
@@ -1,11 +1,14 @@
 import 'express-async-errors'
 import express, { Application } from 'express'
 import cors from 'cors'
+import { handleErrorsMiddleware } from './middlewares/errors/errors.middleware'
 
 const app: Application = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use(handleErrorsMiddleware)
 
 const PORT = 3000
 app.listen(PORT, () => {
