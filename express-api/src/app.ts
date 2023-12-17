9a1d2cf1fd064779134995346685bfa30496eed2
gabriel <gabpavao@outlook.com>
2023-12-17 11:06:38 -0300

    ExpressJS Server Initialized

---
+++ express-api/src/app.ts
@@ -0,0 +1,13 @@
+import 'express-async-errors'
+import express, { Application } from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+
+app.use(express.json())
+app.use(cors())
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT)
+})
